bb23ed07a59019df3d903cd16ab6b14bfcb5718e
Topa2020 <topa666@yahoo.com>
2023-12-08 17:12:19 +0200

    8.14

--- Library-backend/library-backend.js
+++ Library-backend/library-backend.js
@@ -1,7 +1,30 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 const { v1: uuid } = require('uuid')
+const { GraphQLError } = require('graphql')
+const jwt = require('jsonwebtoken')
 
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+const Book = require('./models/book')
+const Author = require('./models/author')
+const User = require('./models/user')
+const author = require('./models/author')
+require('dotenv').config()
+
+const MONGODB_URI = process.env.MONGODB_URI
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB:', error.message)
+  })
+
+/*
 let authors = [
   {
     name: 'Robert Martin',
@@ -27,20 +50,6 @@ let authors = [
     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
   },
 ]
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
-*/
 
 let books = [
   {
@@ -93,16 +102,16 @@ let books = [
     genres: ['classic', 'revolution']
   },
 ]
-
-/*
-  you can remove the placeholder query once your first one has been implemented 
 */
+
+ 
+
 
 const typeDefs = `
   type Book {
     title: String!
     published: Int!
-    author: String!
+    author: Author!
     id: ID!
     genres: [String!]!
   }
@@ -114,15 +123,26 @@ const typeDefs = `
     born: Int
   }
 
+  type User {
+    username: String!
+    favoriteGenre: String
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [AuthorAndBooks!]!
+    me: User
   }
 
   type AuthorAndBooks {
-    name: String!
+    name: String
     born: Int
     bookCount: Int!
   }
@@ -144,14 +164,29 @@ const typeDefs = `
       born: Int!
     ): Author
 
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+
+    login(
+      username: String!
+      password: String!
+    ): Token
+
   }
 `
 
+
 const resolvers = {
+  
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args) => {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: () => Author.collection.countDocuments(),
+    
+    allBooks: async (root, args) => {
+
+      let books = await Book.find({}).populate('author')
       if(args.author && !args.genre) {
         return books.filter(book => args.author === book.author)
       }
@@ -166,42 +201,123 @@ const resolvers = {
       }
 
     },
-    allAuthors: () => authors.map(author => ({
-        ...author,
-        bookCount: books.filter(book => book.author === author.name).length
+
+    
+
+    allAuthors: async () => {
+      const authors = await Author.find({})
+      const books = await Book.find({})
+      console.log(books)
+      console.log(authors)
+      return authors.map(author => ({
+        ...author.toObject(),
+        bookCount: books.filter(book => book.author.toString() === author._id.toString()).length
       })
       )
+    }
   },
     
   Mutation: {
-    addBook: (root, args) => {
-      const writers = authors.map(a => a.name)
-      if(!writers.includes(args.author)) {
+    addBook: async (root, args, context) => { 
+      let author = await Author.findOne({ name: args.author })
+      const currentUser = context.currentUser
+
+      if(!currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          }
+        })
+      }
+      try {
+      if(!author) {
         const newAuthor = {
           name: args.author,
-          id: uuid(),
           born: null
         }
-        authors = authors.concat(newAuthor)
+        author = new Author({ ...newAuthor })
+        await author.save()
+        
       }
-      const book = { ...args, id: uuid() }
-      books = books.concat(book)
-      return book
-    },
+      
+      const book = new Book({ ...args, author: author._id })
+      
+        await book.save()
+        return book
+      } catch (error) {
+          console.error('Error adding book:', error)
+          throw new GraphQLError('Error adding book', {
+            extensions: {
+              code: 'INTERNAL_SERVER_ERROR',
+              error: error.message
+            }
+          })
+    }},
 
-    editAuthor: (root, args) => {
-      const author = authors.find(a => a.name === args.name)
+    editAuthor: async (root, args, context) => {
+      const currentUser = context.currentUser
+      if(!currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          }
+        })
+      }
+      const author = await Author.findOne({ name: args.name })
+    
       if(!author) {
         return null
       }
-      const updatedAuthor = { ...author, born: args.setBornTo}
-      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
-      return updatedAuthor
+      author.born = args.setBornTo
+      try {
+        await author.save()
+      } catch(error){
+          throw new GraphQLError('Error updating birth year')
+      }
+      return author
     },
 
     setBornTo: (root, args) => {
       return args
+    },
+
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username, favoriteGenre: args.favoriteGenre })
+
+      return user.save()
+        .catch(error => {
+          throw new GraphQLError('Creating user failed', {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: args.name,
+              invalidArgs: args.favoriteGenre,
+              error
+            }
+          })
+        })
+    },
+
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if ( !user || args.password !== 'secret' ) {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT'
+          }
+        })
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id
+      }
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
     }
+
+
+    
   }
 }
 
@@ -212,6 +328,17 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.startsWith('Bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), process.env.JWT_SECRET
+      )
+      const currentUser = await User
+        .findById(decodedToken.id)
+      return { currentUser }
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,7 +1,26 @@
 import { useState } from 'react'
-import { useQuery } from '@apollo/client'
+import Select from 'react-select'
+import { useMutation } from '@apollo/client'
+import { UPDATE_AUTHOR } from '../queries'
 
 const Authors = ({ show, authors }) => {
+  const [selectedOption, setSelectedOption] = useState(null)
+  const [born, setBirthyear] = useState('')
+
+  const [ updateAuthor ] = useMutation(UPDATE_AUTHOR)
+
+  const options = authors.map((a) => ({value: a.name, label: a.name}))
+
+  const submit = async (event) => {
+    event.preventDefault()
+    
+    const name = selectedOption.value
+    if(Number.isInteger(born)) {
+      updateAuthor({ variables: { name, born }})
+    } 
+    setBirthyear('')
+    setSelectedOption(null)
+  }
   
   if (!show) {
       return null
@@ -26,6 +45,23 @@ const Authors = ({ show, authors }) => {
             ))}
           </tbody>
         </table>
+        <h3>Set birthyear</h3>
+        <form onSubmit={submit}>
+        <div>
+          <Select
+            value={selectedOption}
+            onChange={setSelectedOption}
+            options={options}
+          />
+        </div>
+        <div>
+          born <input value={born}
+            onChange={({ target }) => setBirthyear(Number(target.value))}
+          />
+        </div>
+        
+        <button type='submit'>update author</button>
+      </form>
       </div>
     )
   }
